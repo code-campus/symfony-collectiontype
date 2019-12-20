093c9b80350401aba964f08381e68f5148945762
Arnaud BODEL <contact@osw3.net>
2019-12-20 12:14:02 +0100

    -

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Authors;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,24 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('nickname')
+            
+            /* Firstname */
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => ['class' => "form-control"]
+            ])
+            
+            /* Lastname */
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => ['class' => "form-control"]
+            ])
+            
+            /* Nickname */
+            ->add('nickname', TextType::class, [
+                'label' => "Pseudo",
+                'attr' => ['class' => "form-control"]
+            ])
         ;
     }
 
